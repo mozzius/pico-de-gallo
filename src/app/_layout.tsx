0ca33df0f78928f85c0a8a68d145f09b8a8891ae
Samuel Newman <mozzius@protonmail.com>
2024-10-10 09:47:35 +0300

    look into using expo-auth-session

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -5,11 +5,13 @@ import {
 } from "@react-navigation/native";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { useColorScheme } from "#/lib/useColorScheme";
+import { ResponseType, useAuthRequest } from "expo-auth-session";
 import { Stack } from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
-import { useEffect } from "react";
+import { useCallback, useEffect } from "react";
 import "react-native-reanimated";
 import "#/global.css";
+import { Button } from "react-native";
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
@@ -18,6 +20,17 @@ const queryClient = new QueryClient();
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
+  const [request, result, prompt] = useAuthRequest({
+    clientId: "https://pico.mozzius.dev/client-metadata.json",
+    redirectUri: "picodegallo:///",
+    scopes: ["atproto", "transition:generic"],
+    usePKCE: true,
+    responseType: ResponseType.Code,
+  }, null)
+
+  const headerLeft = useCallback(() => {
+    return <Button title="Log in" onPress={() => prompt()} />
+  }, [])
 
   useEffect(() => {
     SplashScreen.hideAsync();
@@ -35,6 +48,7 @@ export default function RootLayout() {
               headerShadowVisible: true,
               headerBlurEffect: "prominent",
               headerStyle: { backgroundColor: "rgba(255,255,255,0.001)" },
+              headerLeft,
             }}
           />
           <Stack.Screen name="+not-found" />
