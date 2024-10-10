868a41c9419f368513d3b3054117437354c677b4
Samuel Newman <mozzius@protonmail.com>
2024-10-10 10:10:18 +0300

    Update client.ts

--- api/_utils/client.ts
+++ api/_utils/client.ts
@@ -14,7 +14,7 @@ export const createClient = async () => {
       scope: "atproto transition:generic",
       grant_types: ["authorization_code", "refresh_token"],
       response_types: ["code"],
-      application_type: "web",
+      application_type: "native",
       token_endpoint_auth_method: "none",
       dpop_bound_access_tokens: true,
     },
